be8faafac5611063618222dfdc3d94c921b662aa
aidinem <aidintaghavian20@gmail.com>
2021-01-06 10:29:07 +0330

    Adding owlcarousel and create slide product and adding fonts

--- js/script.js
+++ js/script.js
@@ -11,3 +11,37 @@ window.addEventListener("scroll",amin)
     }
 };
 /////////////////////
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    nav: true,
+    rtl: true,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+});
